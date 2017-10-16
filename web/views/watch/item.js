6ae515376388c0f0ab985814e71039dff0376259
Pierre-Élie Fauché <github@pierre-elie.me>
2017-10-15 23:10:56 -0400

    SOLD badge

--- web/views/watch/item.js
+++ web/views/watch/item.js
@@ -1,10 +1,11 @@
 import BaseView from 'views/_base';
+import classNames from 'classnames';
 
 export default class WatchItem extends BaseView {
   render() {
     const { watch } = this.props;
     return (
-      <a href={watch.url} target="_blank" className="watch">
+      <a href={watch.url} target="_blank" className={classNames('watch', {sold: watch.sold})}>
         <div className="images">
           <div className="image" style={{'background-image': `url(${watch.primaryImageUrl})`}}></div>
           {watch.secondaryImageUrl ? <div className="image secondary" style={{'background-image': `url(${watch.secondaryImageUrl})`}}></div> : undefined}
