eb27160aa2f41242b0b135db30f33f8288e9540d
Pierre-Élie Fauché <github@pierre-elie.me>
2017-10-18 19:48:04 -0400

    watches: throttle call to next page

--- web/views/watch/list.js
+++ web/views/watch/list.js
@@ -3,15 +3,24 @@ import WatchItem from 'views/watch/item';
 import InfiniteScroll from 'react-infinite-scroller';
 
 export default class WatchList extends BaseView {
+  constructor() {
+    super();
+    this.fetchNextPage = _.throttle(this.fetchNextPage.bind(this), 3000);
+  }
+
+  fetchNextPage() {
+    this.props.watches.fetchNextPage();
+  }
+
   render() {
-    const { watches } = this.props
+    const { watches } = this.props;
 
     return (
       <div className="watches absolute-cover">
         <h2>C O N T R A B A N D</h2>
         <InfiniteScroll
           hasMore={watches.hasNextPage()}
-          loadMore={watches.fetchNextPage.bind(watches)}
+          loadMore={this.fetchNextPage.bind(this)}
           initialLoad={false}
           loader={<div className="loader">Loading ...</div>}
           useWindow={true}
